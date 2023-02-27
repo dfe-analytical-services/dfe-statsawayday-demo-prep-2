0f6d96734da7f30b8cf217b6318b49f501e1496d
Rich Bielby <richard.bielby@education.gov.uk>
2023-02-27 16:10:31 +0000

    Adapted existing line plot

--- R/plotting.R
+++ R/plotting.R
@@ -1,7 +1,7 @@
-createAvgRevTimeSeries <- function(df, inputArea) {
-  ggplot(df, aes(x = year, 
-                 y = average_revenue_balance, 
-                 color = area_name)) +
+createTimeSeriesPlot <- function(df, breakdown, metric) {
+  ggplot(df, aes(x = time_period, 
+                 y = get(metric), 
+                 color = get(breakdown))) +
     geom_line(size = 1.2) +
     theme_classic() +
     theme(
@@ -12,41 +12,12 @@ createAvgRevTimeSeries <- function(df, inputArea) {
       legend.position = "top"
     ) +
     scale_y_continuous(
-      labels = scales::number_format(accuracy = 1, big = ",", prefix = "£")
+      labels = scales::number_format(accuracy = 1, big = ",")
     ) +
     xlab("Academic year end") +
-    ylab("Average revenue balance") +
+    ylab(metric) +
     scale_color_manual(
-      "Area",
-      breaks = unique(c("England", inputArea)),
       values = gss_colour_pallette
     )
 }
 
-plotAvgRevBenchmark <- function(dfRevenueBalance, inputArea) {
-  ggplot(dfRevenueBalance, aes(
-    x = area_name,
-    y = average_revenue_balance,
-    fill = area_name
-  )) +
-    geom_col() +
-    theme_classic() +
-    theme(
-      text = element_text(size = 12),
-      axis.text.x = element_text(angle = 300),
-      axis.title.x = element_blank(),
-      axis.title.y = element_text(margin = margin(r = 12)),
-      axis.line = element_line(size = 1.0),
-      legend.position = "none"
-    ) +
-    scale_y_continuous(
-      labels = scales::number_format(accuracy = 1, big = ",", prefix = "£")
-    ) +
-    xlab("Area") +
-    ylab("Average revenue balance") +
-    scale_fill_manual(
-      "Area",
-      breaks = unique(dfRevenueBalance$area_name),
-      values = gss_colour_pallette
-    )
-}
